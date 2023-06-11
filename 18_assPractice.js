49cd06434be8635e1e9072b6f007b3c373105ceb
SwanandDundune <sdundune@gmail.com>
2023-06-11 12:53:48 +0530

    commit

--- 18_assPractice.js
+++ 18_assPractice.js
@@ -20,14 +20,18 @@ console.log(newResult);
  console.log(`------------------------------------------------------------------------------`);
   
 const array = [11,3,4,11,4,7,3];
-// const duplicates = [];
+const nonDuplicate = [];
+const duplicates = [];
 for (let i = 0; i < array.length; i++) {
          for (let j = i +1 ; j < array.length; j++) {
             if (array[i]==array[j]) {
-              array.push(array[i]);
+              duplicates.push(array[i]);
+            }else{
+              nonDuplicate.push(array[i])
             }
             
          }
 }
-console.log(arr);
+console.log(duplicates);
+
 
